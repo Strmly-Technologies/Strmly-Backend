1cf21efbdba47cef67124d559088485a0fba83ae
RohithBN <rohithbn27@gmail.com>
2025-08-12 11:34:21 +0530

    common access util

--- controller/community.controller.js
+++ controller/community.controller.js
@@ -5,6 +5,8 @@ const User = require('../models/User')
 const { handleError, uploadImageToS3 } = require('../utils/utils')
 const { checkCreatorPassAccess } = require('../controller/creatorpass.controller')
 const UserAccess = require('../models/UserAccess')
+const { checkAccess } = require('../utils/access')
+
 
 const CreateCommunity = async (req, res, next) => {
   const { name, bio, type, amount, fee_description } = req.body
@@ -530,96 +532,7 @@ const getCommunityProfileDetails = async (req, res, next) => {
     handleError(error, req, res, next)
   }
 }
-const checkAccess = async (video, userId) => {
-  try {
-    if (video.type === 'Free') {
-      video.access = {
-        isPlayable: true,
-        freeRange: {
-          start_time: video.start_time || 0,
-          display_till_time: video.display_till_time || 0,
-        },
-        isPurchased: true,
-        accessType: 'free',
-      }
-    } else if (video.type === 'Paid') {
-      // Check if user has creator pass access
-      const hasCreatorPass = await checkCreatorPassAccess(
-        userId,
-        video.created_by._id.toString()
-      )
 
-      if (hasCreatorPass.hasAccess) {
-        video.access = {
-          isPlayable: true,
-          freeRange: {
-            start_time: video.start_time || 0,
-            display_till_time: video.display_till_time || 0,
-          },
-          isPurchased: true,
-          accessType: 'creator_pass',
-        }
-      } else {
-        // Check if user has purchased the video
-        const hasPurchasedVideo = await UserAccess.findOne({
-          user_id: userId,
-          content_id: video._id,
-          content_type: 'video',
-          access_type: 'paid',
-        })
-
-        if (hasPurchasedVideo) {
-          video.access = {
-            isPlayable: true,
-            freeRange: {
-              start_time: video.start_time || 0,
-              display_till_time: video.display_till_time || 0,
-            },
-            isPurchased: true,
-            accessType: 'purchased',
-          }
-        } else {
-          video.access = {
-            isPlayable: false,
-            freeRange: {
-              start_time: video.start_time || 0,
-              display_till_time: video.display_till_time || 0,
-            },
-            isPurchased: false,
-            accessType: 'limited',
-            price: video.amount || 0,
-          }
-        }
-      }
-    } else {
-      // Default access for unknown type
-      video.access = {
-        isPlayable: false,
-        freeRange: {
-          start_time: video.start_time || 0,
-          display_till_time: video.display_till_time || 0,
-        },
-        isPurchased: false,
-        accessType: 'unknown',
-      }
-    }
-
-    return video
-  } catch (error) {
-    console.error('Error checking video access:', error)
-    // Return video with limited access if error occurs
-    video.access = {
-      isPlayable: false,
-      freeRange: {
-        start_time: video.start_time || 0,
-        display_till_time: video.display_till_time || 0,
-      },
-      isPurchased: false,
-      accessType: 'error',
-    }
-    return video
-  }
-}
 const getCommunityVideos = async (req, res, next) => {
   try {
     const communityId = req.params.id
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,6 +8,8 @@ const { getRedisClient } = require('../config/redis')
 const CreatorPass = require('../models/CreatorPass')
 const Comment = require('../models/Comment')
 const Series = require('../models/Series')
+const { checkAccess } = require('../utils/access')
+
 const GetUserFeed = async (req, res, next) => {
   try {
     const userId = req.user.id.toString()
@@ -329,97 +331,7 @@ const GetUserCommunities = async (req, res, next) => {
 
 const { checkCreatorPassAccess } = require('./creatorpass.controller')
 
-// Add checkAccess function
-const checkAccess = async (video, userId) => {
-  try {
-    if (video.type === 'Free') {
-      video.access = {
-        isPlayable: true,
-        freeRange: {
-          start_time: video.start_time || 0,
-          display_till_time: video.display_till_time || 0,
-        },
-        isPurchased: true,
-        accessType: 'free',
-      }
-    } else if (video.type === 'Paid') {
-      // Check if user has creator pass access
-      const hasCreatorPass = await checkCreatorPassAccess(
-        userId,
-        video.created_by._id.toString()
-      )
 
-      if (hasCreatorPass.hasAccess) {
-        video.access = {
-          isPlayable: true,
-          freeRange: {
-            start_time: video.start_time || 0,
-            display_till_time: video.display_till_time || 0,
-          },
-          isPurchased: true,
-          accessType: 'creator_pass',
-        }
-      } else {
-        // Check if user has purchased the video
-        const hasPurchasedVideo = await UserAccess.findOne({
-          user_id: userId,
-          content_id: video._id,
-          content_type: 'video',
-          access_type: 'paid',
-        })
-
-        if (hasPurchasedVideo) {
-          video.access = {
-            isPlayable: true,
-            freeRange: {
-              start_time: video.start_time || 0,
-              display_till_time: video.display_till_time || 0,
-            },
-            isPurchased: true,
-            accessType: 'purchased',
-          }
-        } else {
-          video.access = {
-            isPlayable: false,
-            freeRange: {
-              start_time: video.start_time || 0,
-              display_till_time: video.display_till_time || 0,
-            },
-            isPurchased: false,
-            accessType: 'limited',
-            price: video.amount || 0,
-          }
-        }
-      }
-    } else {
-      // Default access for unknown type
-      video.access = {
-        isPlayable: false,
-        freeRange: {
-          start_time: video.start_time || 0,
-          display_till_time: video.display_till_time || 0,
-        },
-        isPurchased: false,
-        accessType: 'unknown',
-      }
-    }
-
-    return video
-  } catch (error) {
-    console.error('Error checking video access:', error)
-    // Return video with limited access if error occurs
-    video.access = {
-      isPlayable: false,
-      freeRange: {
-        start_time: video.start_time || 0,
-        display_till_time: video.display_till_time || 0,
-      },
-      isPurchased: false,
-      accessType: 'error',
-    }
-    return video
-  }
-}
 
 const GetUserVideos = async (req, res, next) => {
   try {
